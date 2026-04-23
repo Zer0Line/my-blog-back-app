c903c94f4db4bb828a8a916bece061a5e245ffa8
Sergey <ubxakep@gmail.com>
2026-04-23 20:50:49 +0300

    fix pom add comments

--- src/main/java/com/training/blog/WebBlogApplication.java
+++ src/main/java/com/training/blog/WebBlogApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebConfiguration {
+public class WebBlogApplication {
     public static void main(String[] args) {
-        SpringApplication.run(WebConfiguration.class, args);
+        SpringApplication.run(WebBlogApplication.class, args);
     }
 }
